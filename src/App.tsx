755611785aef4576c45196b58280bf6e69885ace
Anthony Shannon <ashannon1990@gmail.com>
2023-11-01 20:12:16 -0600

    adding function to pull daily forecast from response

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import { useEffect, useState } from 'react';
 import { CurrentWeather,  HourlyChart, HourlyForecast, Card, Container, Button, } from './Components';
 import env from 'react-dotenv';
 import axios from 'axios';
-import { getCurrentWeather, getHourlyWeather } from './resources/getWeather';
+import { getCurrentWeather, getDailyWeather, getHourlyWeather } from './resources/getWeather';
 import { currentWeatherObj, hourlyWeatherObj } from './resources/stateTemplates';
 import './App.scss';
 
 function App() {
 
   const [currentWeather, setCurrentWeather] = useState(currentWeatherObj);
-  const [hourlyForecast, setHourlyForecast] = useState(hourlyWeatherObj)
+  const [hourlyForecast, setHourlyForecast] = useState(hourlyWeatherObj);
+  const [dailyForecast, setDailyForecast] = useState({});
 
   const getWeather = async () => {
     axios.get(`https://api.openweathermap.org/data/3.0/onecall?units=imperial&lat=38.8339578&lon=-104.825348&exclude=minutely&appid=${env.API_KEY}`)
       .then(response => {
         setCurrentWeather(getCurrentWeather(response.data))
         setHourlyForecast(getHourlyWeather(response.data))
+        setDailyForecast(getDailyWeather(response.data))
       })
       .catch(error => {
         console.error(error)
--- src/resources/getWeather.js
+++ src/resources/getWeather.js
@@ -16,8 +16,58 @@ export const getCurrentWeather = (weather) => {
     return currentWeather;
 }
 
+export const getDailyWeather = (weather) => {
+    const dailyWeather = [];
+    const daily = weather.daily;
+    daily.forEach(currentDay => {
+        const date = new Date(currentDay.dt * 1000).toLocaleDateString();
+        const sunrise = new Date(currentDay.sunrise * 1000).toLocaleTimeString();
+        const sunset = new Date(currentDay.sunset * 1000).toLocaleTimeString();
+        const moonrise = new Date(currentDay.moonrise * 1000).toLocaleTimeString();
+        const moonset = new Date(currentDay.moonset * 1000).toLocaleTimeString();
+        const currentDayObj = {
+            date,
+            sunrise,
+            sunset,
+            moonrise,
+            moonset,
+            moonPhase: currentDay.moon_phase,
+            temp: {
+                day: currentDay.temp.day,
+                night: currentDay.temp.night,
+                morning: currentDay.temp.morn,
+                evening: currentDay.temp.eve,
+                min: currentDay.temp.min,
+                max: currentDay.temp.max,
+            },
+            feelsLike: {
+                day: currentDay.feels_like.day,
+                night: currentDay.feels_like.night,
+                evening: currentDay.feels_like.eve,
+                morning: currentDay.feels_like.morn,
+            },
+            pressure: currentDay.pressure,
+            humidity: currentDay.humidity,
+            dewPoint: currentDay.dew_point,
+            windSpeed: currentDay.wind_speed,
+            windDirection: currentDay.wind_deg,
+            windGust: currentDay.wind_gust,
+            weather: {
+                main: currentDay.weather[0].main,
+                description: currentDay.weather[0].description,
+                icon: currentDay.weather[0].icon,
+            },
+            clouds: currentDay.clouds,
+            chancePrecipitaion: currentDay.pop,
+            uvIndex: currentDay.uvi,
+        }
+        dailyWeather.push(currentDayObj)
+    })
+    return dailyWeather;
+}
+
 export const getHourlyWeather = (weather) => {
-    const hourlyWeather = []
+    const hourlyWeather = [];
     const hourly = weather.hourly;
     hourly.forEach(currentHour => {
         const date = new Date(currentHour.dt * 1000).toLocaleDateString();
