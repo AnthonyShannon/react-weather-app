10ce64f2329593b9fc65217a1a66c4a6cb1e5cad
Anthony Shannon <ashannon1990@gmail.com>
2023-11-02 20:19:02 -0600

    starting daily forecast component

--- src/Components/DailyForecast/DailyForecast.tsx
+++ src/Components/DailyForecast/DailyForecast.tsx
@@ -1,10 +1,23 @@
 import { DailyForecastTypes } from '../../resources/interfaces'
+import { Card, WeatherIcon } from '../../Components'
 import './DailyForecast.scss'
 
 const DailyForecast = ({dailyForecast}: DailyForecastTypes) => {
     return (
-        <>
-        </>
+        <Card>
+            {dailyForecast.map(currentDay => (
+                <>
+                <div className='current-day'>
+                    <span>
+                        {currentDay.date}
+                    </span>
+                    <WeatherIcon iconCode={currentDay.weather.icon} />
+                    <span>{`${currentDay.temp.max}/${currentDay.temp.min}\u00B0F`}</span>
+                    <span>{currentDay.weather.description}</span>
+                </div>
+                </>
+            ))}
+        </Card>
     )
 }
 
